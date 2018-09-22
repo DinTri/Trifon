b22071807d2709316c1c896cfcfd99b98d5f9cb1
TriDin <tpd_bg@yahoo.com>
2018-09-22 14:43:14 +0200

    Added Store viles

---
+++ Store/Model/StoreContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Store.Model
+{
+    public partial class StoreContext : DbContext
+    {
+        public StoreContext()
+        {
+        }
+
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Stories> Stories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\Melissa;Database=Store;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Stories>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Plot)
+                    .HasColumnName("plot")
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Upvotes).HasColumnName("upvotes");
+
+                entity.Property(e => e.Writer)
+                    .HasColumnName("writer")
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
---
+++ Store/Model/Stories.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Model
+{
+    public partial class Stories
+    {
+        public int Id { get; set; }
+        public string Plot { get; set; }
+        public string Writer { get; set; }
+        public int? Upvotes { get; set; }
+    }
+}
